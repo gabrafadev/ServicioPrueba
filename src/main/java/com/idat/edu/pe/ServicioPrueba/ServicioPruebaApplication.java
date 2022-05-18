3a9b3c55741966e9d18b81e0b5cabcd9ba4faba2
gabrafadev <desarrollo.sistemas.rafael@gmail.com>
2022-05-18 17:02:44 -0500

    first commit

---
+++ src/main/java/com/idat/edu/pe/ServicioPrueba/ServicioPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.edu.pe.ServicioPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/edu/pe/ServicioPrueba/ServicioPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.edu.pe.ServicioPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
